423a6c66a974a597d5d2b03979604f51c18c6bb0
Victor <vitchor@gmail.com>
2012-08-19 17:33:40 -0300

    Added static files and fof navigation html file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,4 +1,5 @@
 # Django settings for mysite project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,7 +11,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
+    'default': {    
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'poll',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
@@ -56,18 +57,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = (os.path.join('/Users/marciarozenfeld/DynaFocusServer/mysite/', 'uploader', 'static'),)
 
 # List of finder classes that know how to find static files in
 # various locations.
--- mysite/uploader/urls.py
+++ mysite/uploader/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('uploader.views',
     url(r'^$', 'index'),
     url(r'^image/$', 'image'),
     url(r'^(?P<fof_name>\S+)/fof/$', 'fof'),    
+    url(r'^(?P<device_id_value>\S+)/user/(?P<fof_name_value>\S+)/fof_name/$', 'user_fof'),    
 )
